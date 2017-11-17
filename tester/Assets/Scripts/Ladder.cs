078382c872eca03e59ccbbe79a2113bbf9c0505a
mlarnick <31775845+mlarnick@users.noreply.github.com>
2017-11-17 12:14:19 -0500

    Block level

---
+++ tester/Assets/Scripts/Ladder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour {
+
+    GameObject playerOBJ;
+    bool canClimb = false;
+    float speed = 1;
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            canClimb = true;
+            playerOBJ = coll.gameObject;
+        }
+    }
+
+    void OnCollisionExit(Collision coll2)
+    {
+        if (coll2.gameObject.tag == "Player")
+        {
+            canClimb = false;
+            playerOBJ = null;
+        }
+    }
+    void Update()
+    {
+        if (canClimb)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                playerOBJ.transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                playerOBJ.transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * speed);
+            }
+        }
+    }
+}
---
+++ tester/Assets/Scripts/Platform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    Vector3 startingPosition;
+
+    void Start()
+    {
+        startingPosition = gameObject.transform.position;    
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float timeValue = Time.time;
+        transform.position = new Vector3(startingPosition.x, Mathf.PingPong(timeValue, 16) + 7.5f, startingPosition.z);
+    }
+}
